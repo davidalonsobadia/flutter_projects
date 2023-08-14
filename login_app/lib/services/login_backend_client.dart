211380eb2c077fd2fee0b29f7734b62c203561f2
David Alonso <david.alonsobadia@gmail.com>
2023-08-14 10:51:20 +0200

    Small update

--- login_app/lib/services/login_backend_client.dart
+++ login_app/lib/services/login_backend_client.dart
@@ -31,7 +31,7 @@ class LoginBackendClient {
         },
       );
       return returnResponseOrThrowException(response);
-    } on IOException catch (e) {
+    } on IOException {
       throw NetworkException('There was a Network disruption. Please try again later.');
     }
   }
@@ -48,7 +48,7 @@ class LoginBackendClient {
       );
 
       return returnResponseOrThrowException(response);
-    } on IOException catch (e) {
+    } on IOException {
       throw NetworkException('There was a Network disruption. Please try again later.');
     }
   }
